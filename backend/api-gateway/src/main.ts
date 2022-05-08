27122b0051f55d86edc6471d3235c5280950099a
Víctor Gómez <victorgomez1209@outlook.es>
2022-05-08 14:57:57 +0000

    wip:  add kanban microservice

--- backend/api-gateway/src/main.ts
+++ backend/api-gateway/src/main.ts
@@ -1,12 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@vira/app.module';
-// import { ExceptionFilter } from '@vira/common/filters/exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.useGlobalFilters(new ExceptionFilter());
   app.enableCors({
-    origin: 'http://192.168.1.142:8080',
+    origin: 'http://192.168.1.132:8080',
     credentials: true,
   });
   await app.listen(3000);
--- frontend/src/components/templates/kanban.template.tsx
+++ frontend/src/components/templates/kanban.template.tsx
@@ -1,42 +1,113 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import "@vira/components/templates/styles/kanban.template.css";
 
 export const KanbanLayout = () => {
   return (
     <div className="h-100 row flex-grow-1">
       <div className="col-md-2 shadow-sm">
-        <ul className="list-unstyled">
-          <li>
-            <button
-              className="btn btn-toggle align-items-center rounded collapsed"
-              data-vds-toggle="collapse"
-              data-vds-target="#home-collapse"
-              aria-expanded="false"
-            >
-              Espacios de trabajo
-            </button>
-            <div className="collapse" id="home-collapse">
-              <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 ps-3 small">
-                <li>
-                  <a href="#" className="link-dark rounded">
-                    Overview
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="link-dark rounded">
-                    Updates
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="link-dark rounded">
-                    Reports
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </li>
-          <li>Espacios de trabajo</li>
-        </ul>
+        <div className="flex-shrink-0 pt-2 bg-white" style={{ width: "280px" }}>
+          <ul className="list-unstyled ps-0">
+            <li className="mb-1">
+              <button
+                className="btn btn-toggle align-items-center rounded collapsed"
+                data-vds-toggle="collapse"
+                data-vds-target="#home-collapse"
+                aria-expanded="true"
+              >
+                Home
+              </button>
+              <div className="collapse show" id="home-collapse">
+                <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                  <li>
+                    <a href="#" className="link-dark rounded">
+                      Overview
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="link-dark rounded">
+                      Updates
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="link-dark rounded">
+                      Reports
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li className="mb-1">
+              <button
+                className="btn btn-toggle align-items-center rounded collapsed"
+                data-vds-toggle="collapse"
+                data-vds-target="#dashboard-collapse"
+                aria-expanded="false"
+              >
+                Dashboard
+              </button>
+              <div className="collapse" id="dashboard-collapse">
+                <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                  <li>
+                    <a href="#" className="link-dark rounded">
+                      Overview
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="link-dark rounded">
+                      Weekly
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="link-dark rounded">
+                      Monthly
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="link-dark rounded">
+                      Annually
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li className="mb-1">
+              <button
+                className="btn btn-toggle align-items-center rounded collapsed"
+                data-vds-toggle="collapse"
+                data-vds-target="#orders-collapse"
+                aria-expanded="false"
+              >
+                Orders
+              </button>
+              <div className="collapse" id="orders-collapse">
+                <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                  <li>
+                    <a href="#" className="link-dark rounded">
+                      New
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="link-dark rounded">
+                      Processed
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="link-dark rounded">
+                      Shipped
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="link-dark rounded">
+                      Returned
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li className="border-top my-3"></li>
+          </ul>
+        </div>
       </div>
       <div className="col-md-10">{<Outlet />}</div>
     </div>
